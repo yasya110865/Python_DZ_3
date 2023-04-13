3dc1f51818a44a29c6aa57e3537d147f567e3353
yasya110865 <evn04@mail.ru>
2023-04-13 14:26:58 +0600

    first commit

--- 3seg.py
+++ 3seg.py
@@ -14,7 +14,7 @@ list_1 = input('Введите элементы списка через запя
 list_1 = list_1.split(',')
 list_2 = input('Введите элементы списка через запятую: ')
 list_2 = list_2.split(',')
-for i in list_1:
+for i in list_1[:]:
     if i in list_2:
         list_1.remove(i)
 print(list_1)
